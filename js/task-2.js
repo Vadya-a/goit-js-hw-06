b0d7ddf23978a699f25049579c3be085b6322ab1
Vadim Ramkovich <vadimka.ramkovich@gmail.com>
2026-09-03 21:04:54 +0200

    all task

--- js/task-2.js
+++ js/task-2.js
@@ -49,7 +49,6 @@ console.log(galleryItemsEl);
 const style = document.createElement("style"); 
 style.textContent = ` .gallery 
 { display: flex; flex-wrap: wrap; gap: 24px; padding: 0; list-style: none; }
-  .gallery li { width: calc((100% - 48px) / 2); }
   .gallery img { display: block; width: 100%; height: 200px;  } `;
    
    document.head.append(style);
